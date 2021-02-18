d1da3f75abc1c1ec1cfcc1881eda4d756bf3882a
Federico Berbara <federicomberbara@gmail.com>
2021-02-18 00:45:24 -0300

    Create service folder that will interact with mongoose

---
+++ backend/src/services/budgetService.js
@@ -0,0 +1,17 @@
+const Budget = require('../models/Budget');
+
+const createOperation = async () => {};
+const editOperation = async () => {};
+const deleteOperation = async () => {};
+const getOperations = async () => {};
+const getOperation = async () => {};
+const getBalance = async () => {};
+
+module.exports = {
+    createOperation,
+    editOperation,
+    deleteOperation,
+    getOperations,
+    getOperation,
+    getBalance,
+}
---
+++ backend/src/services/userService.js
@@ -0,0 +1,11 @@
+const Users = require('../models/Users');
+
+const createUser = async () => {};
+const loginUser = async () => {};
+const authUser = async () => {};
+
+module.exports = {
+    createUser,
+    loginUser,
+    authUser,
+};
