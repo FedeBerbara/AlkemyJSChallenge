b31aedfb7a08019fc1f34a2a92c65ad83af6c6de
Federico Berbara <federicomberbara@gmail.com>
2021-02-17 23:47:34 -0300

    Create folders and files for API

---
+++ backend/src/routes/budget.routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router(); 
+const budgetCtrl = require('../controller/budgetCtrl');
+
+router.get();
+router.get();
+router.post();
+router.put();
+router.delete();
+
+module.exports = router;
---
+++ backend/src/routes/users.routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router(); 
+const userCtrl = require('../controller/userCtrl');
+
+router.get();
+router.get();
+router.post();
+router.put();
+router.delete();
+
+module.exports = router;
