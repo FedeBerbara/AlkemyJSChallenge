9b74969c4f96870c0ddb199dab39b66e7bbd54b7
Federico Berbara <federicomberbara@gmail.com>
2021-02-18 00:46:56 -0300

    Minimal changes

--- backend/src/router.js
+++
@@ -1,9 +0,0 @@
-const budgetRoutes = require('./routes/budget.routes');
-const userRoutes = require('./routes/users.routes');
-
-const router = (app) => {
-    //app.use('/api', budgetRoutes);
-    //app.use('/api', userRoutes);
-};
-
-module.exports = router;
