b3a289369d51a6d850ca05228116e18efc93bf6d
Federico Berbara <federicomberbara@gmail.com>
2021-02-17 23:48:50 -0300

    Create basic express server with the main endpoint

---
+++ backend/src/index.js
@@ -0,0 +1,30 @@
+//* Modules
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoDb = require('./database/database');
+const apiRoutes = require('./router');
+
+//* Initializations
+const app = express();
+mongoDb();
+require('dotenv/config');
+
+//* Settings
+const port = process.env.PORT || 5000;
+
+//* Middlewares
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({extended: true}));
+
+//* Routes
+apiRoutes(app);
+
+//* Starting the server 
+app.listen(port, () => {
+    console.log(`Server running in port ${port}`);
+});
---
+++ backend/src/router.js
@@ -0,0 +1,9 @@
+const budgetRoutes = require('./routes/budget.routes');
+const userRoutes = require('./routes/users.routes');
+
+const router = (app) => {
+    //app.use('/api', budgetRoutes);
+    //app.use('/api', userRoutes);
+};
+
+module.exports = router;
