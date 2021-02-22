4ee741598fc37d9aa1b01ec2b8afa4eaafbb8bac
Federico Berbara <federicomberbara@gmail.com>
2021-02-22 00:59:05 -0300

    Create the routes with endpoints

--- backend/src/routes/budget.routes.js
+++ backend/src/routes/budget.routes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router(); 
 const budgetCtrl = require('../controller/budgetCtrl');
 
-router.get();
-router.get();
-router.post();
-router.put();
-router.delete();
+router.get('/', budgetCtrl.getMovements);
+router.get('/id/:id', budgetCtrl.getMovement);
+router.get('/type/:type', budgetCtrl.getMovementsByType);
+router.post('/add', budgetCtrl.createMovement);
+router.put('/update/:id', budgetCtrl.editMovement);
+router.delete('/erase/:id', budgetCtrl.eraseMovement);
 
 module.exports = router;
