5fb7191434c4a61500bd1917d563465a1231fb39
Federico Berbara <federicomberbara@gmail.com>
2021-02-28 21:17:14 -0300

    Create function to fetch data from API

--- frontend/src/pages/Home/Main.jsx
+++ frontend/src/pages/Home/Main.jsx
@@ -5,18 +5,20 @@ import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
 import TransactionForm from './components/TransactionForm';
 import Movements from './components/Movements';
+import Balance from './components/Balance';
+
+//* Services
+import {getMovementsApi} from '../../services/budgetServices'
+
 
 const Main = () => {
 
-    const [ operations, setOperations ] = useState();
+    const [ operations, setOperations ] = useState([]);
 
     const fetchData = async () => {
-        const response = await fetch('http://localhost:5000/api/movements/sorted/sorted');
-        const data = await response.json();
+        const data = await getMovementsApi();
         setOperations(data);
-        console.log(data);
     }
-
 
     useEffect(() => {
         fetchData()
@@ -25,10 +27,18 @@ const Main = () => {
     return (
         <>
             <Navbar />
-                <div className="container-fluid main-container">
+                <div className="container-fluid">
                     <div className="row">
                         <TransactionForm />
-                        <Movements />
+                        <Balance />
+                        {operations.map(i => (
+                            <Movements
+                            id={i._id}
+                            concept={i.concept}
+                            amount={i.amount}
+                            date={i.date}
+                            type={i.type}/>
+                        ))}
                     </div>
                 </div>
             <Footer />
