5885a96022aa16c1560d3af200662506c44fc018
Federico Berbara <federicomberbara@gmail.com>
2021-02-28 21:09:55 -0300

    Add endopints for user authentication

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,7 +1,9 @@
 const budgetRoutes = require('./routes/budget.routes');
+const userRoutes = require('./routes/users.routes');
 
 const router = app => {
     app.use('/api/movements', budgetRoutes);
+    app.use('/api/user', userRoutes);
 };
 
 module.exports = router;
