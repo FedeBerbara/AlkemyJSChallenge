5d8957d2cf9caa582ff2cfc949a6172406aa80c8
Federico Berbara <federicomberbara@gmail.com>
2021-02-28 21:18:13 -0300

    Passing props from father and functions for edit and deleted named

--- frontend/src/pages/Home/components/Movements.jsx
+++ frontend/src/pages/Home/components/Movements.jsx
@@ -1,85 +1,45 @@
-import React from 'react'
+import React from 'react';
+import Swal from 'sweetalert2';
 
 const Movements = (props) => {
 
-    const { id, concept, amount, date, type } = props;
+    const { _id, concept, amount, date, type } = props;
+
+    const id = _id;
+
+    const onEdit = () => {};
+    
+    const onDelete = async id => {
+
+        Swal.fire({
+            title: 'Stop!!',
+            text: '¿Are you sure you want to delete this movement?',
+            showDenyButton: true,
+            showConfirmButton: true,
+            showLoaderOnConfirm: true,
+            showLoaderOnDeny: false,
+            icon: 'warning',
+        });
+
+    };
 
     return (
-        <div className="col-12 col-lg-6 movements-container">
-            <div className="row">
-                <div className="col-12 col-lg-6 card-container-left">
-                    <div className="card-body">
-                        <h5 className="movement-title">{concept}</h5>
-                        <h6 className="movement-type mb-2 text-muted">{type}</h6>
-                        <p className="movement-amount">${amount}</p>
-                        <p className="movement-date">{date}</p>
-                        <button className="card-btn btn btn-outline-info">Edit</button>
-                        <button className="card-btn btn btn-outline-danger ml-2">Delete</button>
-                    </div>
-                    <div className="card-body">
-                        <h5 className="movement-title">Car Insurance</h5>
-                        <h6 className="movement-type mb-2 text-muted">Expenses</h6>
-                        <p className="movement-amount">$ 1500</p>
-                        <button className="card-btn btn btn-outline-info">Edit</button>
-                        <button className="card-btn btn btn-outline-danger ml-2">Delete</button>
-                    </div>
-                    <div className="card-body">
-                        <h5 className="movement-title">Car Insurance</h5>
-                        <h6 className="movement-type mb-2 text-muted">Expenses</h6>
-                        <p className="movement-amount">$ 1500</p>
-                        <button className="card-btn btn btn-outline-info">Edit</button>
-                        <button className="card-btn btn btn-outline-danger ml-2">Delete</button>
-                    </div>
-                    <div className="card-body">
-                        <h5 className="movement-title">Car Insurance</h5>
-                        <h6 className="movement-type mb-2 text-muted">Expenses</h6>
-                        <p className="movement-amount">$ 1500</p>
-                        <button className="card-btn btn btn-outline-info">Edit</button>
-                        <button className="card-btn btn btn-outline-danger ml-2">Delete</button>
-                    </div>
-                    <div className="card-body">
-                        <h5 className="movement-title">Car Insurance</h5>
-                        <h6 className="movement-type mb-2 text-muted">Expenses</h6>
-                        <p className="movement-amount">$ 1500</p>
-                        <button className="card-btn btn btn-outline-info">Edit</button>
-                        <button className="card-btn btn btn-outline-danger ml-2">Delete</button>
-                    </div>
-                </div>
-                <div className="col-12 col-lg-6 card-container-right">
-                    <div className="card-body">
-                        <h5 className="movement-title">Car Insurance</h5>
-                        <h6 className="movement-type mb-2 text-muted">Expenses</h6>
-                        <p className="movement-amount">$ 1500</p>
-                        <button className="card-btn btn btn-outline-info">Edit</button>
-                        <button className="card-btn btn btn-outline-danger ml-2">Delete</button>
-                    </div> 
-                    <div className="card-body">
-                        <h5 className="movement-title">Car Insurance</h5>
-                        <h6 className="movement-type mb-2 text-muted">Expenses</h6>
-                        <p className="movement-amount">$ 1500</p>
-                        <button className="card-btn btn btn-outline-info">Edit</button>
-                        <button className="card-btn btn btn-outline-danger ml-2">Delete</button>
-                    </div>
-                    <div className="card-body">
-                        <h5 className="movement-title">Car Insurance</h5>
-                        <h6 className="movement-type mb-2 text-muted">Expenses</h6>
-                        <p className="movement-amount">$ 1500</p>
-                        <button className="card-btn btn btn-outline-info">Edit</button>
-                        <button className="card-btn btn btn-outline-danger ml-2">Delete</button>
-                    </div>
-                    <div className="card-body">
-                        <h5 className="movement-title">Car Insurance</h5>
-                        <h6 className="movement-type mb-2 text-muted">Expenses</h6>
-                        <p className="movement-amount">$ 1500</p>
-                        <button className="card-btn btn btn-outline-info">Edit</button>
-                        <button className="card-btn btn btn-outline-danger ml-2">Delete</button>
-                    </div>
-                    <div className="card-body">
-                        <h5 className="movement-title">Car Insurance</h5>
-                        <h6 className="movement-type mb-2 text-muted">Expenses</h6>
-                        <p className="movement-amount">$ 1500</p>
-                        <button className="card-btn btn btn-outline-info">Edit</button>
-                        <button className="card-btn btn btn-outline-danger ml-2">Delete</button>
+        <div className="col-12 movements-container">
+            <div className="card">
+                <div className="card-body card-container">
+                    <h5 className="movement-title"> {concept} </h5>
+                    <h6 className="movement-type text-muted"> {type} </h6>
+                    <p className="movement-amount">$ {amount} </p>
+                    <p className="movement-date"> {date} </p>
+                    <div className="btn-container">
+                        <button onClick={onEdit}
+                            className="card-btn btn btn-outline-info">
+                            Edit
+                        </button>
+                        <button onClick={onDelete} 
+                            className="card-btn btn btn-outline-danger ml-2">
+                            Delete
+                        </button>
                     </div>
                 </div>
             </div>
