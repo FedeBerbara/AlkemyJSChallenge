414a4d98e7c1282584f329769b4686c1314beb52
Federico Berbara <federicomberbara@gmail.com>
2021-02-28 21:18:33 -0300

    Create balance component static

---
+++ frontend/src/pages/Home/components/Balance.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Balance = () => {
+    return (
+        <>
+            <div className="col-12">
+                <div className="card-body balance-card">
+                    <h5 className="balance-title"> Actual Balance </h5>
+                    <p className="balance-amount">$ 1500 </p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Balance;
